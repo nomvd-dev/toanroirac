cbf20d1cf5e45240087aa7e461c0c6e0944a362f
kkngayther <leanhtrieu21.09.2k@gmail.com>
2021-12-22 10:33:10 +0700

    Final

--- src/components/DeThi/index.js
+++ src/components/DeThi/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { DeThiPageContainer, DeThiDiv, DeThiContent, DeThiH4, DeThiLink } from './DeThiElements';
 import pdf1 from '../../images/dethi/De_mau_ky_2_2020_2021.pdf';
-import pdf2 from '../../images/dethi/De_on_tap_cuoi_ky.pdf'
+import pdf2 from '../../images/dethi/De_on_tap_cuoi_ky.pdf';
+import pdf3 from '../../images/dethi/De_on_tap_cuoi_ky_DA.pdf';
 
 const DeThiPage = () => {
     return (
@@ -9,12 +10,16 @@ const DeThiPage = () => {
             <DeThiPageContainer />
             <DeThiContent>
                 <DeThiDiv>
-                    <DeThiH4>Đề thi mẫu cuối học kỳ 2, năm học 2020-2021</DeThiH4>
+                    <DeThiH4>Đề thi mẫu cuối học kỳ 2, năm học 2020-2021 + Đáp án</DeThiH4>
                     <DeThiLink href={pdf1} target="_blank" rel="noreferrer">Chi tiết</DeThiLink>
                 </DeThiDiv>
                 <DeThiDiv>
                     <DeThiH4>Đề ôn tập cuối kỳ - Bộ môn Toán Lý</DeThiH4>
                     <DeThiLink href={pdf2} target="_blank" rel="noreferrer">Chi tiết</DeThiLink>
+                </DeThiDiv>
+                <DeThiDiv>
+                    <DeThiH4>Đề ôn tập cuối kỳ - Bộ môn Toán Lý + Đáp án</DeThiH4>
+                    <DeThiLink href={pdf3} target="_blank" rel="noreferrer">Chi tiết</DeThiLink>
                 </DeThiDiv>
             </DeThiContent>
         </>
